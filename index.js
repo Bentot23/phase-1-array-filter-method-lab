9b2c3e113b9b49f507d2b946df109c07d133c1aa
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-01-25 00:27:59 -0600

    commit tree 0df524f592365ab2298c2f592737bf8d2c7c8739

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+// const findMatching = drivers.filter(person => person === "Bobby");
+function findMatching(driver, string) {
+    return driver.filter(function(driver) { 
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+};
+function fuzzyMatch(driver, string) {
+    return driver.filter(driver => driver.toLowerCase().substring(0, string.length) === string.toLowerCase());
+}
+function matchName(driver, string) {
+    return driver.filter(driver => driver.name === string)
+}
